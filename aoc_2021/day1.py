966751499be6d00788588df6b60c0845d60a4afa
martin-martin <breuss.martin@gmail.com>
2021-12-05 14:25:34 +0100

    Solve day 1 part 2/2

--- aoc_2021/day1.py
+++ aoc_2021/day1.py
@@ -3,12 +3,24 @@ from pathlib import Path
 data_dir = Path("/Users/martin/Documents/CodingNomads/community/aoc-2021/data")
 
 depths = [int(d) for d in data_dir.joinpath("day1.txt").read_text().split("\n")]
-print(depths)
+
+WINDOW = 3  # Change window size here
 
 count = 0
 
 for index, depth in enumerate(depths):
-    if (index > 0) and (depth > depths[index - 1]):
+    try:
+        # breakpoint()
+        sliding_window = [depths[index + n] for n in range(WINDOW)]
+        sliding_window_next = [depths[index + n] for n in range(1, WINDOW + 1)]
+        # breakpoint()
+        # print(sliding_window)
+    except IndexError:
+        print(f"No more triplet values. Last item {depth=}, {index=}")
+        break
+    sliding_sum = sum(sliding_window)
+    sliding_sum_next = sum(sliding_window_next)
+    if sliding_sum < sliding_sum_next:
         count += 1
 
 print(count)
--- tests/test_aoc_2021.py
+++ tests/test_aoc_2021.py
@@ -1,5 +1,9 @@
-from aoc_2021 import __version__
+from aoc_2021 import __version__, training
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_two_numbers_add_to_2020():
+    assert training.two_numbers_sum_to_2020([0, 2020]) == (0, 2020)
+    assert training.two_numbers_sum_to_2020([1721, 979, 366, 299, 675, 1456]) == (1721, 299)
